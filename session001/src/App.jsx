52cdc3ff853520ed85fd6e60b7fbcc7d1ff79077
thecassion <pierrerobentz.cassion@gmail.com>
2025-12-01 16:05:31 -0500

    TD01 : Ajouter un composant Header dans la page

--- session001/src/App.jsx
+++ session001/src/App.jsx
@@ -1,43 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import mbdsLogo from './assets/mbds.svg'
 import './App.css'
 
 function Header() {
   return (
     <header>
-      <h1>Bienvenue dans mon application React</h1>
+      {/* logo */}
+      <img src={mbdsLogo} className="mbds-logo" alt="MBDS logo" />
+      {/* title */}
+      <h1>Introduction à React</h1>
+      {/* subtitle */}
+      <h2>A la découverte des premières notions de React</h2>
     </header>
   )
 }
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
     <Header />
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
-      <div className="card">
-
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        MBDS is cool!
-        </p>
-    </>
   )
 }
 
