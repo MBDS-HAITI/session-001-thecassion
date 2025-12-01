b44f562ba23f7e32dfac9e31b849bf01ff4f20b1
thecassion <pierrerobentz.cassion@gmail.com>
2025-12-01 15:38:03 -0500

    feat: restructure App component and enhance header content

--- session001/src/App.jsx
+++ session001/src/App.jsx
@@ -3,11 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function Header() {
+  return (
+    <header>
+      <h1>Bienvenue dans mon application React</h1>
+    </header>
+  )
+}
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Header />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -16,11 +24,14 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Introduction à React</h1>
+      <h2>A la découverte des premières notions de React</h2>
+      <h3>Vite + React</h3>
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
