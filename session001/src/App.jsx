2d78cadaa54ca1e912b92da8a40f37a82cc06366
thecassion <pierrerobentz.cassion@gmail.com>
2025-12-01 16:43:34 -0500

    TD02: Afficher la date et l'heure du jour

--- session001/src/App.jsx
+++ session001/src/App.jsx
@@ -16,9 +16,20 @@ function Header() {
 }
 
 function MainContent() {
+  const [now, setNow] = useState(new Date())
+
+  // update hour every second
+  setTimeout(() => setNow(new Date()), 1000)
+  const Mois = now.toLocaleString('fr-FR', { month: 'long' })
+  const Annee = now.getFullYear()
+  const Heure = String(now.getHours()).padStart(2, '0')
+  const Minute = String(now.getMinutes()).padStart(2, '0')
+  const Second = String(now.getSeconds()).padStart(2, '0')
+  const jourNames = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi']
+  const jour = jourNames[now.getDay()]+' '+ String(now.getDate()).padStart(2, '0')
   return (
     <main>
-      <p>Ici, nous afficherons des informations interessantes :)</p>
+      <p>Bonjour, on est le {jour} , {Mois}, {Annee} et il est {Heure}:{Minute}:{Second}</p>
     </main>
   )
 }
